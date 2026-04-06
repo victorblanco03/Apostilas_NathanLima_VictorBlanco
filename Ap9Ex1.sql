135a7f648db8573419582b35b93582cc36aa00e5
victorblanco <victorblanco03@gmail.com>
2026-04-06 08:37:12 -0300

    Ap9Ex1.sql  Função para gerar valor aleatório entre limite inferior e superior

---
+++ Ap9Ex1.sql
@@ -0,0 +1,7 @@
+-- Função para gerar valor aleatório entre limite inferior e superior
+CREATE OR REPLACE FUNCTION valor_aleatorio_entre(lim_inferior INT, lim_superior INT)
+RETURNS INT AS $$
+BEGIN
+    RETURN FLOOR(RANDOM() * (lim_superior - lim_inferior + 1) + lim_inferior)::INT;
+END;
+$$ LANGUAGE plpgsql;
